e80929778a957a54fea0206e94210f08271afe72
CeschimLuigi <luigifaggeceschim@gmail.com>
2025-01-20 15:35:37 -0300

    project initializated

---
+++ src/test/java/com/luigiCeschim/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigiCeschim.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

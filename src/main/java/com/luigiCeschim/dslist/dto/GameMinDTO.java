cd41f83cdd06feedeaea505f43857ed5aef9d179
CeschimLuigi <luigifaggeceschim@gmail.com>
2025-01-24 13:44:39 -0300

    GET find by list, Query sql,

--- src/main/java/com/luigiCeschim/dslist/dto/GameMinDTO.java
+++ src/main/java/com/luigiCeschim/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.luigiCeschim.dslist.dto;
 
 import com.luigiCeschim.dslist.entities.Game;
+import com.luigiCeschim.dslist.projections.GameMinProjection;
 import jakarta.persistence.Column;
 
 public class GameMinDTO {
@@ -20,6 +21,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/luigiCeschim/dslist/services/GameService.java
+++ src/main/java/com/luigiCeschim/dslist/services/GameService.java
@@ -2,6 +2,7 @@ package com.luigiCeschim.dslist.services;
 
 import com.luigiCeschim.dslist.dto.GameDTO;
 import com.luigiCeschim.dslist.dto.GameMinDTO;
+import com.luigiCeschim.dslist.projections.GameMinProjection;
 import com.luigiCeschim.dslist.repositories.GameRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+
+
     @Transactional(readOnly = true)
     public GameDTO findById(Long id){
         var result = gameRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
@@ -28,6 +31,14 @@ public class GameService {
 
        return result.stream().map(GameMinDTO::new).toList();
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO>findByList(Long listId){
+        var result = gameRepository.searchByList(listId);
+
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
 
 
 }
